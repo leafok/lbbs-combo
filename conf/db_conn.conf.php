d74226e94f96d3c3b6ef73d19fecb39fcc13410d
Leaflet <leaflet@leafok.com>
2026-01-07 00:21:08 +0800

    Add testing / demo configuration

---
+++ conf/db_conn.conf.php
@@ -0,0 +1,7 @@
+<?php
+	$DB_hostname			=	"mysql";
+	$DB_username			=	"lbbs";
+	$DB_password			=	"YjM2ZmRmODNmYmE";
+	$DB_database			=	"lbbs";
+
+	$DB_session_timezone	=	"+08:00"; //Keep consistent with DB server config
---
+++ conf/smtp.conf.php
@@ -0,0 +1,8 @@
+<?php
+	$Mail_Type = "SMTP";
+	$Mail_Server = "localhost";
+	$Mail_Server_Port = 25;
+	$Mail_Auth = false;
+	$Mail_User = "root@localhost";
+	$Mail_Pass = "123456";
+	$Mail_Sender = "root@localhost";
