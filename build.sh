2733398747be7ff9a424b9835d055d73d966a4fe
Leaflet <leaflet@leafok.com>
2026-01-07 16:34:37 +0800

    Update tag of images

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 git submodule update --init --recursive
 
-docker build ./leafok_bbs --file ./leafok_bbs/Dockerfile/dockerfile.apache --tag leafok/lbbs-apache:base
+docker build ./leafok_bbs --file ./leafok_bbs/Dockerfile/dockerfile.apache --tag leafok/lbbs-apache:latest
 docker build ./leafok_bbs --file ./leafok_bbs/Dockerfile/dockerfile.php --tag leafok/lbbs-php:latest
-docker build ./lbbs --file ./lbbs/Dockerfile/dockerfile.bbsd --tag leafok/lbbs-bbsd:base
+docker build ./lbbs --file ./lbbs/Dockerfile/dockerfile.bbsd --tag leafok/lbbs-bbsd:latest
 docker build . --file Dockerfile/dockerfile.apache.testing --tag leafok/lbbs-apache:testing
 docker build . --file Dockerfile/dockerfile.bbsd.testing --tag leafok/lbbs-bbsd:testing
