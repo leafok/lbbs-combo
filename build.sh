86a5e25e33056071d54d64ede12d8724601110b2
Leaflet <leaflet@leafok.com>
2026-01-07 16:48:42 +0800

    Update

--- build.sh
+++ build.sh
@@ -6,4 +6,5 @@ docker build ./leafok_bbs --file ./leafok_bbs/Dockerfile/dockerfile.apache --tag
 docker build ./leafok_bbs --file ./leafok_bbs/Dockerfile/dockerfile.php --tag leafok/lbbs-php:latest
 docker build ./lbbs --file ./lbbs/Dockerfile/dockerfile.bbsd --tag leafok/lbbs-bbsd:latest
 docker build . --file Dockerfile/dockerfile.apache.testing --tag leafok/lbbs-apache:testing
+docker build . --file Dockerfile/dockerfile.php.testing --tag leafok/lbbs-php:testing
 docker build . --file Dockerfile/dockerfile.bbsd.testing --tag leafok/lbbs-bbsd:testing
