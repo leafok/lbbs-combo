7bc633603e8743d1e508819cbba7413cd729b1bf
Leaflet <leaflet@leafok.com>
2026-01-09 17:09:50 +0800

    Use multi-stage build in Dockerfile

--- build.sh
+++ build.sh
@@ -20,16 +20,10 @@ fi
 
 docker buildx inspect multi-platform-builder --bootstrap
 
-docker buildx build ./leafok_bbs --platform=$DOCKER_PLATFORMS --file ./leafok_bbs/Dockerfile/dockerfile.apache --tag leafok/lbbs-apache:latest
-docker buildx build ./leafok_bbs --platform=$DOCKER_PLATFORMS --file ./leafok_bbs/Dockerfile/dockerfile.php --tag leafok/lbbs-php:latest
-docker buildx build ./lbbs --platform=$DOCKER_PLATFORMS --file ./lbbs/Dockerfile/dockerfile.bbsd --tag leafok/lbbs-bbsd:latest
 docker buildx build . --platform=$DOCKER_PLATFORMS --file Dockerfile/dockerfile.apache.testing --tag leafok/lbbs-apache:testing
 docker buildx build . --platform=$DOCKER_PLATFORMS --file Dockerfile/dockerfile.php.testing --tag leafok/lbbs-php:testing
 docker buildx build . --platform=$DOCKER_PLATFORMS --file Dockerfile/dockerfile.bbsd.testing --tag leafok/lbbs-bbsd:testing
 
-docker buildx build ./leafok_bbs --platform=$RUN_PLATFORM --file ./leafok_bbs/Dockerfile/dockerfile.apache --tag leafok/lbbs-apache:latest --load
-docker buildx build ./leafok_bbs --platform=$DOCKER_PLATFORMS --file ./leafok_bbs/Dockerfile/dockerfile.php --tag leafok/lbbs-php:latest --load
-docker buildx build ./lbbs --platform=$DOCKER_PLATFORMS --file ./lbbs/Dockerfile/dockerfile.bbsd --tag leafok/lbbs-bbsd:latest --load
-docker buildx build . --platform=$DOCKER_PLATFORMS --file Dockerfile/dockerfile.apache.testing --tag leafok/lbbs-apache:testing --load
-docker buildx build . --platform=$DOCKER_PLATFORMS --file Dockerfile/dockerfile.php.testing --tag leafok/lbbs-php:testing --load
-docker buildx build . --platform=$DOCKER_PLATFORMS --file Dockerfile/dockerfile.bbsd.testing --tag leafok/lbbs-bbsd:testing --load
+docker buildx build . --platform=$RUN_PLATFORM --file Dockerfile/dockerfile.apache.testing --tag leafok/lbbs-apache:testing --load
+docker buildx build . --platform=$RUN_PLATFORM --file Dockerfile/dockerfile.php.testing --tag leafok/lbbs-php:testing --load
+docker buildx build . --platform=$RUN_PLATFORM --file Dockerfile/dockerfile.bbsd.testing --tag leafok/lbbs-bbsd:testing --load
